58c8333e3042c311f67ee30d5b293f8b43025d21
alice <hime.bandit@gmail.com>
2012-05-25 10:46:57 +0900

    Update mainmenu/MainActivity.java

--- 00_mainmenu/MainActivity.java
+++ 00_mainmenu/MainActivity.java
@@ -1,4 +1,4 @@
-//2012.5.16 現在バグ発生中
+//2012.5.25 画面遷移の動作を確認
 
 package jp.com.disptop;
 
@@ -14,22 +14,26 @@ public class MainActivity extends Activity implements OnClickListener{
 	/** Called when the activity is first created. */
 	
 	//各ボタンを召喚
-	Button button_main = (Button) findViewById(R.id.main_button);
-	Button button_sub1 = (Button) findViewById(R.id.sub1_button);
-	Button button_sub2 = (Button) findViewById(R.id.sub2_button);
+	Button button_main;
+	Button button_sub1;
+	Button button_sub2;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.menu);
+		setContentView(R.layout.main);
+		button_main = (Button) findViewById(R.id.main_button);
+		button_sub1 = (Button) findViewById(R.id.sub1_button);
+		button_sub2 = (Button) findViewById(R.id.sub2_button);
 		
+		button_main.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
 		if(v == button_main){
-			Intent intent = new Intent(this,MainActivity.class);
+			Intent intent = new Intent(this,menuActivity.class);
 			startActivity(intent);
 		}
 	}
