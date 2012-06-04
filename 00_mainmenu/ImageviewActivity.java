21be75d9d25e7872cb927054e9f0390d3d4e9632
alice <hime.bandit@gmail.com>
2012-06-04 12:01:51 +0900

    update file

--- 00_mainmenu/ImageviewActivity.java
+++ 00_mainmenu/ImageviewActivity.java
@@ -8,21 +8,18 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
-public class ImageviewActivity extends Activity {
+public class ImageviewActivity extends Activity{
 	ImageView img;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		img = (ImageView)findViewById(R.id.imageView1);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-//		BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
-//		Bitmap bitmap = wallpaper.getBitmap();
-//		img.setImageBitmap(bitmap);
 		setContentView(R.layout.image);
+		img = (ImageView)findViewById(R.id.imageView1);
+		
+		BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
+		Bitmap bitmap = wallpaper.getBitmap();
+		img.setImageBitmap(bitmap);
 
 	}
 }
