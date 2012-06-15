74df7c0140a732ea22259d676f883979dd7c20a1
alicemagic <hime.bandit@gmail.com>
2012-06-15 10:59:10 +0900

    update file

--- 01_main/ImageviewActivity.java
+++ 01_main/ImageviewActivity.java
@@ -1,16 +1,45 @@
-package jp.com.disptop;
+//////////////////////////////////////////////////
+//		壁紙を取得して、全画面に表示させるためのクラス			//
+/////////////////////////////////////////////////
+
+package jp.com.disp_top;
 
 import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.Display;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class ImageviewActivity extends Activity {
+
+public class ImageviewActivity extends Activity{
+	ImageView img;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.image);
+		
+		//イメージビューを召喚
+		img = (ImageView)findViewById(R.id.imageView1);
+		
+		//ディスプレイ情報を取得するための前準備
+		WindowManager windowManager = getWindowManager();
+		Display display = windowManager.getDefaultDisplay();
+
+		//ディスプレイの高さと幅を取得
+		int height = display.getHeight();
+		int width = display.getWidth();
+		
+		//設定されている壁紙の取得
+		BitmapDrawable wallpaper = (BitmapDrawable) getWallpaper();
+		Bitmap bitmap = wallpaper.getBitmap();
+		
+		//ビットマップに高さと幅を設定
+		bitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+		
+		//イメージビューに取得した壁紙を設定
+		img.setImageBitmap(bitmap);
 
 	}
 }
